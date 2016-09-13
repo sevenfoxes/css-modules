38378c57cd34c97ea464e7c3073af5679893f39c
Scott Nesham <principles-web+sn@asynchrony.com>
2016-09-13 15:09:18 -0500

    configure postcss

--- Gulpfile.js
+++ Gulpfile.js
@@ -8,3 +8,33 @@ const path = require('path');
 const fs = require('fs');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
+
+const p = {
+  src: './src',
+  dist: './dist'
+}
+
+let getJSONFromCssModules = (cssFileName, json) => {
+  const cssName = path.basename(cssFileName, '.css');
+  const jsonFileName = path.resolve(p.dist, `${ cssName }.json`);
+  fs.writeFileSync(jsonFileName, JSON.stringify(json));
+}
+
+let getClass = (module, className) => {
+  const moduleFileName  = path.resolve(p.dist, `${ module }.json`);
+  const classNames      = fs.readFileSync(moduleFileName).toString();
+  return JSON.parse(classNames)[className];
+}
+
+gulp.task('style', () => {
+  gulp.src(`${p.src}/style/*.scss`)
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer,
+      modules({ getJSON: getJSONFromCssModules })
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(p.dist))
+    .pipe(sync.stream());
+});
